b02482b0f7f4a28a626150e4ae857e731948dc80
u shall not pass <jjav_2000@hotmail.com>
2024-05-06 19:21:42 -0500

    Update config.h

--- config.h
+++ config.h
@@ -3,8 +3,8 @@
 #define CONFIG_H
 
 // Wifi AUTH
-const char* ssid = "NETGEAR92";
-const char* password = "YOURSSID";
+const char* ssid = "YOURSSID";
+const char* password = "YOURPASSWORD";
 const char* mqttServer = "YOURMQTTO_IP";
 const int mqttPort = YOURMQTOPORT;
 
@@ -13,16 +13,20 @@ const int ECHO_PIN = 18;
 const int TRIGGER_PIN = 23;
 
 // Similarly, for other constants
-const int RELAY2_PIN = 25;
-const int ONE_WIRE_BUS1 = 26;
-const int ONE_WIRE_BUS2 = 14;
-const int RELAY_PIN = 27;
-const int TURBIDITY_SENSOR_PIN = 13;
-const int RELAY3_PIN = 33;
-const int LIGHT_SENSOR_PIN = 12;
-const int TDS_SENSOR_PIN = 35;
-const int CO2_SENSOR_PIN = 36;
-const int PH_SENSOR_PIN = 39;
+// Can not be used with wifi [25,26,27,14,12,13,4,2,15]
+// Only input pins [34-39]
+// Available ADC1 pins [36,39,34,35,32,33]
+
+const int RELAY2_PIN = 22;
+const int ONE_WIRE_BUS1 = 16; 
+const int ONE_WIRE_BUS2 = 17;
+const int RELAY_PIN = 19;
+const int TURBIDITY_SENSOR_PIN = 32;          //ADC1_CHANNEL_4
+const int RELAY3_PIN = 33;                    //ADC1_CHANNEL_5
+const int LIGHT_SENSOR_PIN = 34;              //ADC1_CHANNEL_6
+const int TDS_SENSOR_PIN = 35; //input only   //ADC1_CHANNEL_7
+const int CO2_SENSOR_PIN = 36; //input only   //ADC1_CHANNEL_0
+const int PH_SENSOR_PIN = 39; //input only    //ADC1_CHANNEL_3
 const float SEALEVELPRESSURE_HPA = 1010.01;
 const int MAX_SENSOR_VALUE = 4095;
 const int MAX_RETRIES = 5;
@@ -32,3 +36,5 @@ const float MAX_DISTANCE = 109.22;
 const float MIN_TANK_DISTANCE = 70.0;
 
 #endif
+
+
