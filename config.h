50d0455136d93f3b3db1c1faac23ee18379eae64
u shall not pass <jjav_2000@hotmail.com>
2024-05-24 21:35:26 -0500

    Update config.h

--- config.h
+++ config.h
@@ -2,6 +2,14 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+#include <driver/adc.h>
+#include <esp_adc_cal.h>
+
+#define DEFAULT_VREF    1100        // Default Vref in mV (use 1100 unless you've measured a different value)
+#define NO_OF_SAMPLES   64          // Multisampling
+
+esp_adc_cal_characteristics_t *adc_chars;
+
 // Wifi AUTH
 const char* ssid = "YOURSSID";
 const char* password = "YOURPASSWORD";
@@ -17,13 +25,15 @@ const int TRIGGER_PIN = 23;
 // Only input pins [34-39]
 // Available ADC1 pins [36,39,34,35,32,33]
 
-const int RELAY2_PIN = 22;
+const int CO2_TX_PIN = 1;  // TX pin for MH-Z19B
+const int CO2_RX_PIN = 3;  // RX pin for MH-Z19B
 const int ONE_WIRE_BUS1 = 16; 
 const int ONE_WIRE_BUS2 = 17;
 const int RELAY_PIN = 19;
+const int RELAY2_PIN = 22;
 const int TURBIDITY_SENSOR_PIN = 32;          //ADC1_CHANNEL_4
 const int RELAY3_PIN = 33;                    //ADC1_CHANNEL_5
-const int LIGHT_SENSOR_PIN = 34;              //ADC1_CHANNEL_6
+const int LIGHT_SENSOR_PIN = 34; //input only //ADC1_CHANNEL_6
 const int TDS_SENSOR_PIN = 35; //input only   //ADC1_CHANNEL_7
 const int CO2_SENSOR_PIN = 36; //input only   //ADC1_CHANNEL_0
 const int PH_SENSOR_PIN = 39; //input only    //ADC1_CHANNEL_3
@@ -36,5 +46,3 @@ const float MAX_DISTANCE = 109.22;
 const float MIN_TANK_DISTANCE = 70.0;
 
 #endif
-
-
